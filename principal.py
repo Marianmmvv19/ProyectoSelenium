06470555f2831fdebb6a3aafc6ba728b13123f50
marian vasquez <marianmarcelavasquez@gmail.com>
2024-09-29 14:02:06 -0400

    Se añadio una pausa de 10 segundos para que cargue la pagina

--- principal.py
+++ principal.py
@@ -1,15 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import time
 # Configurar el controlador de Firefox (geckodriver debe estar en el PATH)
 driver = webdriver.Firefox()
 
 #abrir google.com
 driver.get('https://computoalcaldes-int.oep.org.bo')
 
-
 # Esperar unos segundos para ver los resultados
-#driver.implicitly_wait(60)
+time.sleep(10)
 
 # Cerrar el navegador
-#driver.quit()
+driver.quit()
