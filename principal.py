737f00a49bce2e73baed31642a71066040ddec92
marian vasquez <marianmarcelavasquez@gmail.com>
2024-09-30 15:13:11 -0400

    Se añadio mensajes por consola y tambien click en el boton actas

--- principal.py
+++ principal.py
@@ -9,9 +9,16 @@ driver.get('https://computoalcaldes-int.oep.org.bo')
 
 #esperar unos segundos para ver los resultados
 time.sleep(10)
+print('Click en el boton')
 
 #click en el boton actas
-actas_button = driver.find_element(By.XPATH, "//span[text()='ACTAS']")
+actas_button = driver.find_element(By.XPATH, "//span[contains(text(), 'ACTAS')]")
+actas_button.click()
 
+print('Esperar unos segundos')
+#esperar unos segundos
+time.sleep(10)
+
+print('Cerrar')
 # Cerrar el navegador
 driver.quit()
